a97e12b8eb87e2ce6ecef595136ec10beb7b9b5a
Geonwoong Park <80371835+geonwoong-creator@users.noreply.github.com>
2023-10-24 16:29:34 +0900

    Update 4_🔎_검색.py

--- pages/4_🔎_검색.py
+++ pages/4_🔎_검색.py
@@ -16,52 +16,52 @@ st.set_page_config(
 
 
 st.header("Search 🔎")
-# search_query = st.text_input("질문하기")
+search_query = st.text_input("질문하기")
 
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "assistant", "content": "안녕하세요. 저는 웹 검색이 가능한 챗봇입니다. 무엇을 도와드릴까요?"}
-    ]
 
 # if "messages" not in st.session_state:
-#     st.chat_message["messages"] = [
+#     st.session_state["messages"] = [
 #         {"role": "assistant", "content": "안녕하세요. 저는 웹 검색이 가능한 챗봇입니다. 무엇을 도와드릴까요?"}
 #     ]
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+# # if "messages" not in st.session_state:
+# #     st.chat_message["messages"] = [
+# #         {"role": "assistant", "content": "안녕하세요. 저는 웹 검색이 가능한 챗봇입니다. 무엇을 도와드릴까요?"}
+# #     ]
 
-if prompt := st.chat_input(placeholder="지금 대한민국 대통령은 누구야?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+# for msg in st.session_state.messages:
+#     st.chat_message(msg["role"]).write(msg["content"])
+
+# if prompt := st.chat_input(placeholder="지금 대한민국 대통령은 누구야?"):
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+#     st.chat_message("user").write(prompt)
     
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        llm = OpenAI(temperature=0)
-        tool_name = ["serpapi"]
-        tools = load_tools(tool_name)
-        agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
-        response = agent.run(st.session_state.messages, callbacks=[st_cb])
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        st.write(response)
+#     with st.chat_message("assistant"):
+#         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+#         llm = OpenAI(temperature=0)
+#         tool_name = ["serpapi"]
+#         tools = load_tools(tool_name)
+#         agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+#         response = agent.run(st.session_state.messages, callbacks=[st_cb])
+#         st.session_state.messages.append({"role": "assistant", "content": response})
+#         st.write(response)
 
 
-# if st.button("질문하기"):
-#     if not search_query.strip():
-#         st.write(f"질문을 입력해주세요.")
-#     else :
-#         try:
-#             # Initialize the OpenAI module, load the SerpApi tool, and run the search query using an agent
-#             llm=OpenAI(temperature=0,verbose=True)
-#             tools = load_tools(["serpapi"], llm)
-#             agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+if st.button("질문하기"):
+    if not search_query.strip():
+        st.write(f"질문을 입력해주세요.")
+    else :
+        try:
+            # Initialize the OpenAI module, load the SerpApi tool, and run the search query using an agent
+            llm=OpenAI(temperature=0,verbose=True)
+            tools = load_tools(["serpapi"], llm)
+            agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
 
-#             result = agent.run(search_query)
+            result = agent.run(search_query)
             
-#             st.write(result)
-#         except Exception as e:
-#             st.write(f"An error occurred: {e}")
+            st.write(result)
+        except Exception as e:
+            st.write(f"An error occurred: {e}")
 
 
 
